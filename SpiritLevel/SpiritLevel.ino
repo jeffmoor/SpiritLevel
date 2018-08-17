1f9ab17ec10887e0cb38433922d57d458a30d38d
Jeff <Jeff@JEFFLAPTOP>
2018-08-17 11:46:55 -0700

    Add project files.

---
+++ SpiritLevel/SpiritLevel.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SpiritLevel.ino
+ Created:	8/17/2018 11:46:48 AM
+ Author:	Jeff
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
