e44cc4d7d5a158f5283d50b81ae6bfbe218ce93a
Jeff <Jeff@JEFFLAPTOP>
2018-08-19 16:01:10 -0700

    Clean up

--- SpiritLevel/SpiritLevel.ino
+++ SpiritLevel/SpiritLevel.ino
@@ -7,12 +7,10 @@
 //Declaring some global variables
 static boolean			set_gyro_angles;
 
-static int				gyro_x, gyro_y, gyro_z,
-						temperature,
-						lcd_loop_counter,
+static int				lcd_loop_counter,
 						angle_pitch_buffer, angle_roll_buffer;
 
-static long				acc_x, acc_y, acc_z, acc_total_vector,
+static long				acc_total_vector,
 						gyro_x_cal, gyro_y_cal, gyro_z_cal,
 						loop_timer;
 
@@ -27,6 +25,10 @@ LiquidCrystal			lcd(7, 8, 9, 10, 11, 12);		// LiquidCrystal(rs, enable, d4, d5, 
 //	setup
 ////////////////////////////////////////////////////////////
 void setup() {
+
+	int				gyro_x, gyro_y, gyro_z, temperature;
+	long			acc_x, acc_y, acc_z;
+
 
 	Wire.begin();												//Start I2C as master
 
@@ -57,7 +59,7 @@ void setup() {
 	for (int cal_int = 0; cal_int < GYRO_CAL_ITERATIONS; cal_int++) {
 		if (cal_int % GYRO_CAL_DOT_PERIOD == 0)
 			lcd.print(".");										// Print a dot on the LCD every 125 readings
-		read_mpu_6050_data();
+		read_mpu_6050_data(gyro_x, gyro_y, gyro_z, temperature, acc_x, acc_y, acc_z);
 		gyro_x_cal += gyro_x;
 		gyro_y_cal += gyro_y;
 		gyro_z_cal += gyro_z;
@@ -87,41 +89,47 @@ void setup() {
 ////////////////////////////////////////////////////////////
 void loop() {
 
-	read_mpu_6050_data();                                                //Read the raw acc and gyro data from the MPU-6050
+	int				gyro_x, gyro_y, gyro_z, temperature;
+	long			acc_x, acc_y, acc_z;
 
-	gyro_x -= gyro_x_cal;                                                //Subtract the offset calibration value from the raw gyro_x value
-	gyro_y -= gyro_y_cal;                                                //Subtract the offset calibration value from the raw gyro_y value
-	gyro_z -= gyro_z_cal;                                                //Subtract the offset calibration value from the raw gyro_z value
 
-	//Gyro angle calculations  [0.0000611 = 1 / (250Hz / 65.5)]
-	angle_pitch += gyro_x * 0.0000611;                                   //Calculate the traveled pitch angle and add this to the angle_pitch variable
-	angle_roll += gyro_y * 0.0000611;                                    //Calculate the traveled roll angle and add this to the angle_roll variable
+	// Read the raw acc, temp, and gyro data from the MPU-6050
+	read_mpu_6050_data(gyro_x, gyro_y, gyro_z, temperature, acc_x, acc_y, acc_z);
 
-	// 0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
-	angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the roll angle to the pitch angel
-	angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the pitch angle to the roll angel
+	// Subtract the gyros' offset calibration values from the raw values
+	gyro_x -= gyro_x_cal;
+	gyro_y -= gyro_y_cal;
+	gyro_z -= gyro_z_cal;
 
-	// Accelerometer angle calculations
-	acc_total_vector = sqrt((acc_x*acc_x) + (acc_y*acc_y) + (acc_z*acc_z));  //Calculate the total accelerometer vector
-																			 //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-	angle_pitch_acc = asin((float)acc_y / acc_total_vector)* 57.296;	//Calculate the pitch angle
-	angle_roll_acc = asin((float)acc_x / acc_total_vector)* -57.296;	//Calculate the roll angle
+	// Gyro angle calculations  [0.0000611 = 1 / (250Hz / 65.5)]
+	angle_pitch += gyro_x * 0.0000610687;                                // Calculate the traveled pitch angle and add this to the angle_pitch variable
+	angle_roll += gyro_y * 0.0000610687;                                 // Calculate the traveled roll angle and add this to the angle_roll variable
+
+	// 0.000001066 = 0.0000611 * (3.142 / 180deg) The Arduino sin function is in radians
+	angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               // If the IMU has yawed transfer the roll angle to the pitch angel
+	angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               // If the IMU has yawed transfer the pitch angle to the roll angel
+
+	// Accelerometer angle calculations 
+	acc_total_vector = sqrt((acc_x*acc_x) + (acc_y*acc_y) + (acc_z*acc_z));  // Calculate the total accelerometer vector
+																			 // 57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+	angle_pitch_acc = asin((float)acc_y / acc_total_vector) * 57.296;	// Calculate the pitch angle
+	angle_roll_acc = asin((float)acc_x / acc_total_vector) * -57.296;	// Calculate the roll angle
 
 	// Place the MPU-6050 spirit level on a level surface and note the values in the following two lines for calibration
-	angle_pitch_acc -= 0.0;                                             //Accelerometer calibration value for pitch
-	angle_roll_acc -= 0.0;                                              //Accelerometer calibration value for roll
+	angle_pitch_acc -= 0.0;                                             // Accelerometer calibration value for pitch
+	angle_roll_acc -= 0.0;                                              // Accelerometer calibration value for roll
 
-	if (set_gyro_angles) {												//If the IMU is already started
-		angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;  //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
-		angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;     //Correct the drift of the gyro roll angle with the accelerometer roll angle
+	if (set_gyro_angles) {												// If the IMU is already started
+		angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;  // Correct the drift of the gyro pitch angle with the accelerometer pitch angle
+		angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;     // Correct the drift of the gyro roll angle with the accelerometer roll angle
 	}
-	else {                                                              //At first start
-		angle_pitch = angle_pitch_acc;                                  //Set the gyro pitch angle equal to the accelerometer pitch angle
-		angle_roll = angle_roll_acc;                                    //Set the gyro roll angle equal to the accelerometer roll angle
-		set_gyro_angles = true;                                         //Set the IMU started flag
+	else {                                                              // At first start
+		angle_pitch = angle_pitch_acc;                                  // Set the gyro pitch angle equal to the accelerometer pitch angle
+		angle_roll = angle_roll_acc;                                    // Set the gyro roll angle equal to the accelerometer roll angle
+		set_gyro_angles = true;                                         // Set the IMU started flag
 	}
 
-	//To dampen the pitch and roll angles a complementary filter is used
+	// To dampen the pitch and roll angles a complementary filter is used
 	angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;   //Take 90% of the output pitch value and add 10% of the raw pitch value
 	angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;      //Take 90% of the output roll value and add 10% of the raw roll value
 
@@ -135,9 +143,8 @@ void loop() {
 ////////////////////////////////////////////////////////////
 //	read_mpu_6050_data
 ////////////////////////////////////////////////////////////
-void read_mpu_6050_data()
+void read_mpu_6050_data(int &gy_x, int &gy_y, int &gy_z, int &temp, long &ac_x, long &ac_y, long &ac_z)
 {
-
 	Wire.beginTransmission(0x68);
 	Wire.write(0x3B);
 	Wire.endTransmission();
@@ -145,15 +152,15 @@ void read_mpu_6050_data()
 
 	while (Wire.available() < 14);						// Wait until all the bytes are received
 
-	acc_x = Wire.read() << 8 | Wire.read();       
-	acc_y = Wire.read() << 8 | Wire.read();       
-	acc_z = Wire.read() << 8 | Wire.read();
+	ac_x = Wire.read() << 8 | Wire.read();       
+	ac_y = Wire.read() << 8 | Wire.read();       
+	ac_z = Wire.read() << 8 | Wire.read();
 
-	temperature = Wire.read() << 8 | Wire.read();
+	temp = Wire.read() << 8 | Wire.read();
 
-	gyro_x = Wire.read() << 8 | Wire.read();      
-	gyro_y = Wire.read() << 8 | Wire.read();      
-	gyro_z = Wire.read() << 8 | Wire.read();      
+	gy_x = Wire.read() << 8 | Wire.read();      
+	gy_y = Wire.read() << 8 | Wire.read();      
+	gy_z = Wire.read() << 8 | Wire.read();      
 }
 
 
